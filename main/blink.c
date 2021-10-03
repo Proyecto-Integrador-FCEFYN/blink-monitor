29563d44971a6117c9caa7a7b8d280388d4fd84d
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-10-03 01:19:10 -0300

    fix example, working now

--- main/blink.c
+++ main/blink.c
@@ -12,17 +12,9 @@
 #include "freertos/semphr.h"
 #include "pthread.h"
 
-
-// #include <pthread.h>
-#include "driver/gpio.h"
 #include "sdkconfig.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-
-
+#define REENTRANT
 
 #define NUMREC 10
 #define NUMPROC 5
@@ -36,18 +28,18 @@ void pidoRec (int n, int id) {
     pthread_mutex_lock(&mutex);
     while (recdis < n)
     {
-        printf(" Trabajador %d pide %d −− espera\n", id, n);
+        printf("Trabajador %d Core %d, pide %d −− espera\n", id, xPortGetCoreID(), n);
         pthread_cond_wait(&espera, &mutex);
     }
-    recdis = recdis - 1;
-    printf("Trabajador %d pide %d -- coge\n", id, n);
+    recdis = recdis - n;
+    printf("Trabajador %d Core %d, pide %d -- toma\n", id, xPortGetCoreID(), n);
     pthread_mutex_unlock(&mutex);
 }
 
 void devuelvoRec (int n, int id) {
     pthread_mutex_lock(&mutex);
     recdis = recdis + n;
-    printf("Trabajador %d devuelve %d", id, n);
+    printf("Trabajador %d Core %d, devuelve %d\n", id, xPortGetCoreID(), n);
     pthread_cond_broadcast(&espera);
     pthread_mutex_unlock(&mutex);
 }
@@ -55,7 +47,8 @@ void devuelvoRec (int n, int id) {
 void *cliente(void *arg) {
     int miid = (int) arg;
     int d, i;
-    for ( i = 0; i < 10; i++)
+    // for ( i = 0; i < 10; i++)
+    while(1)
     {
         d = rand() % 10;
         pidoRec(d, miid);
@@ -74,18 +67,18 @@ void app_main(void)
     pthread_attr_init(&atrib);
     // Ver si existen equivalencias para esto.
     // pthread_attr_setscope(&atrib,PTHREAD_SCOPE_SYSTEM);
-    // i = pthread_attr_setschedpolicy(&atrib,SCHED_RR);
+    //i = pthread_attr_setschedpolicy(&atrib,SCHED_RR);
     printf("%d\n",i);
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&espera, NULL);
  
-    for (i = 0; i < 5; i++)
+    for (i=0;i<5;i++)
     {
         pthread_create(&c[i], &atrib, cliente, (void*) i) ;
     }
     
-    for (i = 0; i < 5; i++)
+    for (i=0;i<5;i++)
     {
         pthread_join(c[i], NULL);
     }
