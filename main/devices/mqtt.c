05f9877c93a39affd5c41f3fad57a79f7d95d60d
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-11-16 22:54:30 -0300

    Esqueleto MQTT esqueleto

---
+++ main/devices/mqtt.c
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 16/11/21.
+//
+
+#include "mqtt.h"
---
+++ main/devices/mqtt.h
@@ -0,0 +1,8 @@
+//
+// Created by agustin on 16/11/21.
+//
+
+#ifndef BLINK_MQTT_H
+#define BLINK_MQTT_H
+
+#endif //BLINK_MQTT_H
