5f574514c823c84031c6253ae8af2e6095cca7eb
Agustin Carranza <gobiernocba\20369477367@tfs.gobiernocba.gov.ar>
2021-11-21 23:52:50 -0300

    Subo archivo de manejador de eventos para futura refactorizacion de codigo.

---
+++ main/manejador_eventos/manejador_eventos.c
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 21/11/21.
+//
+
+#include "manejador_eventos.h"
---
+++ main/manejador_eventos/manejador_eventos.h
@@ -0,0 +1,8 @@
+//
+// Created by agustin on 21/11/21.
+//
+
+#ifndef BLINK_MANEJADOR_EVENTOS_H
+#define BLINK_MANEJADOR_EVENTOS_H
+
+#endif //BLINK_MANEJADOR_EVENTOS_H
